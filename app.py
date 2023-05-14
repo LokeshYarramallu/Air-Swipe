d36248bc0887fdf3fc6eb7ecd9c1d852c1828d3a
Madan2248c <117735689+Madan2248c@users.noreply.github.com>
2023-05-14 17:12:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,16 @@ import pyCode
 app = Flask(__name__)
 port = 3001
 
+
 def mad():
     pyCode.main()
     time.sleep(0)  # Yield to the event loop
 
+
 @app.route('/')
 def index():
     return send_file('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -25,27 +28,29 @@ def upload():
 
     if 'pdfFile' in request.files:
         pdf_file = request.files['pdfFile']
-        file_path = os.path.join(os.getcwd(), pdf_file.filename)
+        file_path = os.path.join(os.getcwd(), secure_filename(pdf_file.filename))
         pdf_file.save(file_path)
         if os.path.exists(file_path):
+            madThread = Thread(target=mad)
+            madThread.start()
             return send_file(file_path)
-            madPromise = mad()
-            await madPromise
         else:
             return "File not found", 404
     elif link != "":
         if link.startswith("<i"):
+            madThread = Thread(target=mad)
+            madThread.start()
             return link
-            madPromise = mad()
-            await madPromise
         else:
             return "Enter a valid link"
     else:
         return "Please select a file to upload"
 
+
 @app.route('/Home', methods=['GET'])
 def home():
     return send_file('Home.html')
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=port)
